6a75c8a59ae48ffc3ff3c677870b5e45672870f2
Pawel2022 <pawelmajerowski.07@gmail.com>
2022-10-17 19:23:12 +0200

    Add new files

---
+++ wpisanie.py
@@ -0,0 +1,129 @@
+print("1 - kwadrat 2 - prostokat 3 - trojkat 4 - trojkat(rownoboczny) 5 - trapez 6 - kolo 7 - romb(z h) 8 - romb(przekatne) 9 - rownoleglobok 10 - V szescianu 11 - V graniastoslupa(pole trojkata (a*h/2)) 12 - V prostopadloscianu 13 - V ostroslupa(podstawa kwadrat) 14 - V walca  15 - V kuli 16 - V stozka 17 - Pc szescianu 18 - Pc graniastoslupa(podstawa kwadratu) 19 - Pc Prostopadloscianu  20 - Pc ostroslupa prawidłowego 21 - Pc walca 22 - Pc stozka 23 - V Toroida(donata) 24 - pole pentagonu 25 - pole hexagonu foremnego 26 - pole heptagonu foremnego 27 -  pole dwudziestokata foremnego 28 - pole owalu 29 - 'liczba uczniów' 30 - przekatna kwadratu 31 - wysokosc trojkata rownobocznego 32 - obw kola")
+qq = float(input())
+if qq == 1:
+    from wzory import p_kwa
+    p_kwa()
+
+if qq == 2:
+    from wzory import p_pro
+    p_pro()
+
+if qq == 3:
+    from wzory import p_t
+    p_t()
+
+if qq == 4:
+    from wzory import p_tr
+    p_tr()
+
+if qq == 5:
+    from wzory import p_tra
+    p_tra()
+
+if qq == 6:
+    from wzory import p_kol
+    p_kol()
+
+if qq == 7:
+    from wzory import p_rombh
+    p_rombh()
+
+if qq == 8:
+    from wzory import p_rombp
+    p_rombp()
+
+if qq == 9:
+    from wzory import p_row
+    p_row()
+
+if qq == 10:
+    from wzory import v_sze
+    v_sze()
+
+if qq == 11:
+    from wzory import v_gra1
+    v_gra1()
+
+if qq == 12:
+    from wzory import v_gra2
+    v_gra2()
+
+if qq == 13:
+    from wzory import v_ostr
+    v_ostr()
+
+if qq == 14:
+    from wzory import v_wal
+    v_wal()
+
+if qq == 15:
+    from wzory import v_kul
+    v_kul()
+
+if qq == 16:
+    from wzory import v_stoz
+    v_stoz()
+
+if qq == 17:
+    from wzory import pc_sze
+    pc_sze()
+
+if qq == 18:
+    from wzory import pc_gran
+    pc_gran()
+
+if qq == 19:
+    from wzory import pc_pro
+    pc_pro()
+
+if qq == 20:
+    from wzory import pc_ostr
+    pc_ostr()
+
+if qq == 21:
+    from wzory import pc_wal
+    pc_wal()
+
+if qq == 22:
+    from wzory import pc_sto
+    pc_sto()
+
+if qq == 23:
+    from wzory import v_tro
+    v_tro()
+
+if qq == 24:
+    from wzory import p_pen
+    p_pen()
+
+if qq == 25:
+    from wzory import p_hex
+    p_hex()
+
+if qq == 26:
+    from wzory import p_hep
+    p_hep()
+
+if qq == 27:
+    from wzory import p_20
+    p_20()
+
+if qq == 28:
+    from wzory import p_owa
+    p_owa()
+
+if qq == 29:
+    from wzory import l_ucz
+    l_ucz()
+
+if qq == 30:
+    from wzory import prz_kw
+    prz_kw()
+
+if qq == 31:
+    from wzory import wtr
+    wtr()
+
+if qq == 32:
+    from wzory import obw_k
+    obw_k()
---
+++ wzory.py
@@ -0,0 +1,152 @@
+from math import pi
+
+def p_kwa():
+    a = int(input("a:"))
+    print(a*a)
+
+def p_pro():
+    y = None
+    a = float(input("a:"))
+    b = float(input('b:'))
+    print(a*b)
+
+def p_t():
+    a = float(input("a:"))
+    h = float(input("h:"))
+    print(a*h%2)
+
+def p_tr():
+    a = float(input("a:"))
+    print(a**2*1,732050807568877/4)
+
+def p_tra():
+    a = float(input('a:'))
+    b = float(input('b:'))
+    h = float(input('h:'))
+    print([a+b]*h%2)
+
+def p_kol():
+    r = float(input("r:"))
+    print(pi*r**2)
+
+def p_rombh():
+    a = float(input("a:"))
+    h = float(input("h:"))
+    print(a*h)
+
+def p_rombp():
+    e = float(input("e:"))
+    f = float(input("f:"))
+    print(e*f%2)
+
+def p_row():
+    a = float(input("a:"))
+    h = float(input("h:"))
+    print(a*h)
+
+def v_sze():
+    a = float(input('a:'))
+    print(a**3)
+
+def v_gra1():
+    a = float(input("a:"))
+    h = float(input("h:"))
+    H = float(input("H:"))
+    print(a*h%H)
+
+def v_gra2():
+    a = float(input("a:"))
+    H = float(input("H:"))
+    print(a*a*H)
+
+def v_ostr():
+    a = float(input("a:"))
+    H = float(input("H:"))
+    print(a**2*H)
+
+def v_wal():
+    r = float(input("r:"))
+    H = float(input("H:"))
+    print(pi*r**2*H)
+
+def v_kul():
+    r = float(input("r:"))
+    print(4/3*pi*r**3)
+
+
+def v_stoz():
+    r = float(input("r:"))
+    H = float(input("H:"))
+    print(1%3*pi*r**2*H)
+
+def pc_sze():
+    a = float(input("a:"))
+    print(6*a**2)
+
+def pc_gran():
+    a = float(input("a:"))
+    H = float(input("H:"))
+    print(2*a**2+a*H)
+def pc_pro():
+    a = float(input("a:"))
+    b = float(input("b:"))
+    c = float(input("c:"))
+    print(2*a*b+2*a*c+2*b*c)
+
+def pc_ostr():
+    a = float(input("a:"))
+    print(a**2**1,732050807568877%4*4)
+
+def pc_wal():
+    r = float(input("r:"))
+    H = float(input("H:"))
+    print(2*pi*r+2*pi*r*H)
+
+def pc_sto():
+    r = float(input("promień:"))
+    l = float(input("tworząca stożka:"))
+    print(pi*r[r+l])
+
+def v_tro():
+    R = float(input("R:"))
+    r = float(input("r:"))
+    print(2*pi**2*R*r**2)
+
+def p_pen():
+    a = float(input("a:"))
+    print(1.720477400588967*a**2)
+
+def p_hex():
+    a = float(input('a:'))
+    print([3*a**2*1.732050807568877]%2)
+
+def p_hep():
+    a = float(input("a:"))
+    print([a**2*1,732050807568877%4]*7)
+
+def p_20():
+    a = float(input("a:"))
+    print([a**2*1,732050807568877%4]*20)
+    p_20()
+
+def p_owa():
+    a = float(input("a(polos):"))
+    b = float(input("b(polos):"))
+    print(pi*a*b)
+
+def l_ucz():
+    n = float(input("Liczba uczniow:"))
+    print(100*2**n)
+    l_ucz()
+
+def prz_kw():
+    a = float(input())
+    print(a*1.414213562373095)
+
+def wtr():
+    a = float(input("a:"))
+    print(a*1,732050807568877%2)
+
+def obw_k():
+    r = float(input("r:"))
+    print(2*pi*r)
